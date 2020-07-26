72a4b3fbfabb1397de016c8adb2e569ee2950063
Tashona Smith <tashona.smith@gmail.com>
2020-07-25 23:08:48 -0500

    Create server.js, client folder, Nav component

---
+++ google-book-search/client/src/components/Nav/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <a href="/">
+                  Google Books
+                </a>
+                <a href="/">
+                  Search
+                </a>
+                <a href="#">
+                  Saved
+                </a>
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
